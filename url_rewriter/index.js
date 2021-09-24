bbde0fb443ea0adf52cb4a96d9678bd429d859d9
Nick Silverman <nckslvrmn@gmail.com>
2021-09-24 13:32:51 -0400

    no need for rewriter anymore

--- url_rewriter/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = (event, context, callback) => {
-    var request = event.Records[0].cf.request;
-    if ((request.uri != "") || (request.uri != "/")) {
-          request.uri = "/secret";
-        }
-    return callback(null, request);
-};
